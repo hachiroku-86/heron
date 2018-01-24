909cc1efa3cca98d6e194f251ba7b9941e00ad0b
heron thomas <thomas.heron.mk1@gmail.com>
2018-01-24 11:21:06 +0000

    Add new file

---
+++ Slam/Slam 1/Mission PHP/infocmd.php
@@ -0,0 +1,10 @@
+<?php session_start();
+
+echo "Baignoire : ".$_POST['baignoire'];
+echo "</br>";
+echo "</br>";
+echo "Ouistiti : ".$_POST['ouistiti'];
+echo "</br>";
+echo "</br>";
+echo "Courgette : ".$_POST['courgette'];
+?>
