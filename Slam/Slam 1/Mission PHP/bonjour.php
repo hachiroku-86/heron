056d5af2481813748aebc9f2e0de67deebe09c2d
heron thomas <thomas.heron.mk1@gmail.com>
2018-01-24 11:19:58 +0000

    Add new file

---
+++ Slam/Slam 1/Mission PHP/bonjour.php
@@ -0,0 +1,18 @@
+<?php 
+echo "bonjour";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+<title>mon titre</title>
+</head>
+<body> 
+</br>
+</br>
+<?php 
+require_once'footer.html';
+?> 
+</body> </br></br>
+<a href="formulaire.html">formulaire</a>
+</html>
